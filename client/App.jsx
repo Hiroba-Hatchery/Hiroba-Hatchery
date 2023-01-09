ed9dda9409198e11452adccfd99938d32f8b88e8
Andy <andyoh.94@gmail.com>
2023-01-09 15:40:07 -0800

    created initial webpack, package.json, and file structure

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hiroba Hatchery FROM APP</h1>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ client/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+// import store from './ReducersAndStore/store';
+import { Provider } from 'react-redux';
+
+const mountNode = document.getElementById('root');
+const root = createRoot(mountNode);
+
+root.render(
+    // <Provider store={store}>
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+    // {/* </Provider> */}
+)
